e8dfd236ebede0cadd5eab74e53f8a1d2a65a16b
Blessing Tanyaradzwa Hove <blessinghove69@gmail.com>
2024-05-27 07:33:44 +0200

    Residao Voucher API

---
+++ voucher-api/index.js
@@ -0,0 +1,16 @@
+require("dotenv").config();
+const express = require("express");
+const app = express();
+const cors = require("cors");
+const getVoucher = require("./routes/getVoucher");
+
+// middlewares
+app.use(express.json());
+app.use(cors());
+
+// routes
+app.get('/', (req, res) => res.send('Welcome to the Residao Voucher API'));
+app.use("/api/getVoucher", getVoucher);
+
+const port = process.env.PORT || 8080;
+app.listen(port, console.log(`Listening on port ${port}...`));
---
+++ voucher-api/routes/getVoucher.js
@@ -0,0 +1,59 @@
+const router = require("express").Router();
+const { sendToken } = require('../utils/TokenTransaction.js');
+const { celo_alfajores } = require('../utils/Chain.js');
+const Joi = require("joi");
+require('dotenv').config();
+
+// Route to get a user's private key by their email
+router.post("/", async (req, res) => {
+    try {
+        const { error } = validate(req.body);
+        if (error)
+            return res.status(400).send({ message: error.details[0].message });
+
+        const toAddress = req.body.toAddress;
+        const token = req.body.token;
+
+        let stringToken = "123456789";
+        if (token !== stringToken) {
+            return res.status(404).send({ message: "Voucher is Invalid" });
+        }
+
+        const to = toAddress;
+        console.log('Sending token to:', to);
+        const amount = 1;
+        console.log('Amount: ', amount);
+        const privateKey = process.env.PRIVATE_KEY;
+        console.log("privateKey", privateKey);
+        const tokenAddress = process.env.CUSD; // cUSD
+        console.log('Token Address:', tokenAddress);
+
+        const { transaction, receipt } = await sendToken(amount, to, privateKey, tokenAddress)
+            .catch(error => {
+                console.error('Error sending token:', error);
+                res.status(500).send({ message: "Error sending token" });
+                return null;
+            });
+
+        if (transaction && receipt) {
+            console.log('Transaction sent:', transaction);
+            res.send({ transactionHash: `${celo_alfajores.blockExplorerUrl}/tx/${receipt.transactionHash}` });
+        } else {
+            return;
+        }
+
+    } catch (error) {
+        console.error("Failed to send token", error);
+        res.status(500).send({ message: "Internal Server Error" });
+    }
+});
+
+const validate = (data) => {
+    const schema = Joi.object({
+        toAddress: Joi.string().required().label("ToAddress"),
+        token: Joi.string().required().label("Token"),
+    });
+    return schema.validate(data);
+};
+
+module.exports = router;
