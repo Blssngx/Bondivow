6734a2f18440396e87b2f48f965c8259bb2cd430
Blessing Tanyaradzwa Hove <blessinghove69@gmail.com>
2024-06-03 21:17:38 +0200

    chore: Update item images and descriptions

--- residao-ui/pages/api/items.ts
+++ residao-ui/pages/api/items.ts
@@ -4,9 +4,15 @@ import { NextApiRequest, NextApiResponse } from 'next';
 
 const items = [
   { id: 1, name: 'Handcrafted Table', description: 'A beautiful handcrafted table.', type: 'goods', image: 'https://images.unsplash.com/photo-1530018607912-eff2daa1bac4?q=80&w=2071&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D', price: '200.00' },
-  { id: 2, name: 'Gardening Service', description: 'Expert gardening service.', type: 'services', image: '/images/gardening.jpg', price: '50.00' },
+  { id: 2, name: 'Gardening Service', description: 'Expert gardening service.', type: 'services', image: 'https://plus.unsplash.com/premium_photo-1678832237885-ef157b15b8ad?q=80&w=1887&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D', price: '50.00' },
   { id: 3, name: 'Vintage Lamp', description: 'A unique vintage lamp.', type: 'goods', image: 'https://images.unsplash.com/photo-1513506003901-1e6a229e2d15?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D', price: '75.00' },
-  { id: 4, name: 'Plumbing Service', description: 'Reliable plumbing services.', type: 'services', image: '/images/plumbing.jpg', price: '100.00' },
+  { id: 4, name: 'Plumbing Service', description: 'Reliable plumbing services.', type: 'services', image: 'https://images.unsplash.com/photo-1558618666-fcd25c85cd64?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D', price: '100.00' },
+  { id: 5, name: 'Bespoke Chair', description: 'A bespoke chair crafted to perfection.', type: 'goods', image: 'https://images.unsplash.com/photo-1562113530-57ba467cea38?q=80&w=1899&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D', price: '150.00' },
+  { id: 6, name: 'Electrician Service', description: 'Professional electrical installation and repair.', type: 'services', image: 'https://plus.unsplash.com/premium_photo-1682086495085-0a73efeac2b7?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D', price: '80.00' },
+  { id: 7, name: 'Modern Sofa', description: 'A stylish and comfortable modern sofa.', type: 'goods', image: 'https://images.unsplash.com/photo-1512212621149-107ffe572d2f?q=80&w=2080&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D', price: '300.00' },
+  { id: 8, name: 'House Cleaning', description: 'Thorough and reliable house cleaning services.', type: 'services', image: 'https://images.unsplash.com/photo-1527515673510-8aa78ce21f9b?q=80&w=1974&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D', price: '60.00' },
+  { id: 9, name: 'Rustic Bed', description: 'A rustic bed made from reclaimed wood.', type: 'goods', image: 'https://images.unsplash.com/photo-1566665797739-1674de7a421a?q=80&w=1974&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D', price: '400.00' },
+  { id: 10, name: 'Painting Service', description: 'Professional painting service for your home.', type: 'services', image: 'https://plus.unsplash.com/premium_photo-1663091832341-cdd716557fa0?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D', price: '90.00' },
 ];
 
 export default function handler(req: NextApiRequest, res: NextApiResponse) {
--- residao-ui/pages/feed/index.tsx
+++ residao-ui/pages/feed/index.tsx
@@ -1,169 +1,104 @@
-// import React, { useEffect, useState } from 'react';
-// import axios from 'axios';
-// import classNames from 'classnames';
-// import BottomNavBar from "@/components/NavBar";
-// import { useAccount } from "wagmi";
-// import { Layer } from "@/components/RoundedDrawerNav";
-// import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs"
-
-// interface Item {
-//     id: number;
-//     name: string;
-//     description: string;
-//     type: 'good' | 'service';
-// }
-
-// const FeedPage: React.FC = () => {
-//     const [items, setItems] = useState<Item[]>([]);
-//     const [activeTab, setActiveTab] = useState<'goods' | 'services'>('goods');
-//     const [userAddress, setUserAddress] = useState("");
-//     const [isMounted, setIsMounted] = useState(false);
-//     const { address, isConnected } = useAccount();
-
-//     useEffect(() => {
-//         setIsMounted(true);
-//     }, []);
-
-//     useEffect(() => {
-//         if (isConnected && address) {
-//             setUserAddress(address);
-//         }
-//     }, [address, isConnected]);
-
-
-//     useEffect(() => {
-//         const fetchItems = async () => {
-//             const response = await axios.get('/api/items'); // Replace with your actual API endpoint
-//             setItems(response.data);
-//         };
-
-//         fetchItems();
-//     }, []);
-
-//     if (!isMounted) {
-//         return null;
-//     }
-
-//     const filteredItems = items.filter(item => item.type === activeTab + 's');
-
-//     return (
-//         <Layer>
-//             <div className="min-h-screen rounded-xl p-4 bg-gray-100">
-//                 {/* <h1 className="text-3xl font-bold text-center mb-4">Community Feed</h1> */}
-//                 <div className="flex justify-center mb-8">
-//                     <button
-//                         className={classNames(
-//                             'px-4 py-2 rounded-l-full',
-//                             activeTab === 'goods' ? 'bg-blue-500 text-white' : 'bg-white text-blue-500'
-//                         )}
-//                         onClick={() => setActiveTab('goods')}
-//                     >
-//                         Goods
-//                     </button>
-//                     <button
-//                         className={classNames(
-//                             'px-4 py-2 rounded-r-full',
-//                             activeTab === 'services' ? 'bg-blue-500 text-white' : 'bg-white text-blue-500'
-//                         )}
-//                         onClick={() => setActiveTab('services')}
-//                     >
-//                         Services
-//                     </button>
-//                 </div>
-//                 <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
-//                     {filteredItems.map(item => (
-//                         <div key={item.id} className="bg-white p-4 rounded-md shadow">
-//                             <h2 className="text-xl font-bold">{item.name}</h2>
-//                             <p className="mt-2">{item.description}</p>
-//                         </div>
-//                     ))}
-//                 </div>
-//             </div>
-//         </Layer>
-//     );
-// };
-
-// export default FeedPage;
-
 import React, { useEffect, useState } from 'react';
 import axios from 'axios';
 import classNames from 'classnames';
 import { Layer } from '@/components/RoundedDrawerNav';
 import { Icons } from '@/components/icons';
-import Image from 'next/image';
 import ItemCard from '@/components/item-card';
 
 interface Item {
-    id: number;
-    name: string;
-    description: string;
-    type: 'goods' | 'services';
-    image: string;
-    price: string;
+  id: number;
+  name: string;
+  description: string;
+  type: 'goods' | 'services';
+  image: string;
+  price: string;
 }
 
 const FeedPage: React.FC = () => {
-    const [items, setItems] = useState<Item[]>([]);
-    const [activeTab, setActiveTab] = useState<'goods' | 'services'>('goods');
+  const [items, setItems] = useState<Item[]>([]);
+  const [activeTab, setActiveTab] = useState<'goods' | 'services'>('goods');
+  const [searchTerm, setSearchTerm] = useState('');
 
-    useEffect(() => {
-        const fetchItems = async () => {
-            const response = await axios.get('/api/items'); // Replace with your actual API endpoint
-            setItems(response.data);
-        };
+  useEffect(() => {
+    const fetchItems = async () => {
+      const response = await axios.get('/api/items'); // Replace with your actual API endpoint
+      setItems(response.data);
+    };
 
-        fetchItems();
-    }, []);
+    fetchItems();
+  }, []);
 
-    const filteredItems = items.filter(item => item.type === activeTab);
+  const filteredItems = items
+    .filter(item => item.type === activeTab)
+    .filter(item => item.name.toLowerCase().includes(searchTerm.toLowerCase()));
 
-    return (
-        <Layer>
-            <div className="min-h-screen rounded-3xl p-2 bg-gray-100">
-                <div className="flex mb-8">
-                    <button
-                        className={classNames(
-                            'px-4 py-2 rounded-full transition-colors flex items-center justify-center contect-place-center`',
-                            activeTab === 'goods' ? "text-black bg-black rounded-full px-3 py-3 " : "text-white"
-                        )}
-                        onClick={() => setActiveTab('goods')}
-                    >
-                        <Icons.goods
-                            fillColor={activeTab === 'goods' ? "white" : "black"}
-                            className="w-5 h-5 "
-                        />
-                        {activeTab === 'goods' && <span className={`${activeTab === 'goods' ? "text-white" : "text-black"} font-bold text-xs uppercase ml-1`}>Goods</span>}
-
-                    </button>
-                    <button
-                        className={classNames(
-                            'px-4 py-2 rounded-full transition-colors flex items-center justify-center contect-place-center`',
-                            activeTab === 'services' ? "text-black  bg-black rounded-full px-3 py-3" : "text-white"
-                        )}
-                        onClick={() => setActiveTab('services')}
-                    >
-                        <Icons.services
-                            fillColor={activeTab === 'services' ? "white" : "black"}
-                            className="w-5 h-5 "
-                        />
-                        {activeTab === 'services' && <span className={`${activeTab === 'services' ? "text-white" : "text-black"} font-bold text-xs uppercase ml-1`}>Services</span>}
-                    </button>
-                </div>
-                <div className="grid grid-cols-2">
-                    {filteredItems.map(item => (
-                        <ItemCard
-                            key={item.id}
-                            id={item.id}
-                            name={item.name}
-                            description={item.description}
-                            image={item.image}
-                            price={item.price}
-                        />
-                    ))}
-                </div>
-            </div>
-        </Layer>
-    );
+  return (
+    <Layer>
+      <div className="min-h-screen rounded-3xl p-2 bg-gray-100">
+        <div className="flex mb-2">
+          <button
+            className={classNames(
+              'px-4 py-2 rounded-full transition-colors flex items-center justify-center',
+              activeTab === 'goods' ? 'text-black bg-black rounded-full px-3 py-3' : 'text-white'
+            )}
+            onClick={() => setActiveTab('goods')}
+          >
+            <Icons.goods
+              fillColor={activeTab === 'goods' ? 'white' : 'black'}
+              className="w-5 h-5"
+            />
+            {activeTab === 'goods' && (
+              <span
+                className={`${activeTab === 'goods' ? 'text-white' : 'text-black'} font-bold text-xs uppercase ml-1`}
+              >
+                Goods
+              </span>
+            )}
+          </button>
+          <button
+            className={classNames(
+              'px-4 py-2 rounded-full transition-colors flex items-center justify-center ml-2',
+              activeTab === 'services' ? 'text-black bg-black rounded-full px-3 py-3' : 'text-white'
+            )}
+            onClick={() => setActiveTab('services')}
+          >
+            <Icons.services
+              fillColor={activeTab === 'services' ? 'white' : 'black'}
+              className="w-5 h-5"
+            />
+            {activeTab === 'services' && (
+              <span
+                className={`${activeTab === 'services' ? 'text-white' : 'text-black'} font-bold text-xs uppercase ml-1`}
+              >
+                Services
+              </span>
+            )}
+          </button>
+        </div>
+        <div className="mb-4">
+          <input
+            type="text"
+            placeholder="Search items..."
+            className="w-full p-2 rounded-full border border-black"
+            value={searchTerm}
+            onChange={(e) => setSearchTerm(e.target.value)}
+          />
+        </div>
+        <div className="grid grid-cols-2 gap-2">
+          {filteredItems.map(item => (
+            <ItemCard
+              key={item.id}
+              id={item.id}
+              name={item.name}
+              description={item.description}
+              image={item.image}
+              price={item.price}
+            />
+          ))}
+        </div>
+      </div>
+    </Layer>
+  );
 };
 
 export default FeedPage;
